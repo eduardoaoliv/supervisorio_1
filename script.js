d434ecdfa0f0b5557d36750e4ac83f31c5e878ef
eduardoaoliv <eduardoalmeidaoliveira@gmail.com>
2021-01-10 14:59:55 -0300

    Mudando o projeto
    os displays passaram a fazer parte do html e não serão mais criados com o click

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var posX;
-var posY;
-var conteiner=document.getElementById("conteiner");
-var i;
+//var posX;
+//var posY;
+//var conteiner=document.getElementById("conteiner");
+//var i;
 
 
+//******************Cria o display motor********************************
+/*
 displayControlMotors();
 function displayControlMotors(){
 	var displayCtrMot=document.getElementsByClassName("motor");
 	posMouse();
 	for(i=0;i<displayCtrMot.length;i++){
-		displayCtrMot[i].addEventListener("click",function(){			
+		displayCtrMot[i].addEventListener("click",function(){
 			var displayMotor=document.createElement("div");
 			var displayMotorId=this.id;//captura o id do botão que é acionado
 			displayMotor.setAttribute("class","display_motor");				
@@ -27,37 +29,36 @@ function displayControlMotors(){
 				display_btnClose.innerHTML="X";
 				display_motorIdent.appendChild(display_btnClose);
 				display_btnClose.addEventListener("click",function(){
-				displayMotor.remove();
+				displayMotor.remove();				
 				});
 					var display_motorBtnLiga=document.createElement("button");
 					display_motorBtnLiga.setAttribute("class","motor_btnLiga");
 					display_motorBtnLiga.setAttribute("id",displayMotorId+"-liga");					
-					display_motorBtnLiga.innerHTML="Liga";					
+					display_motorBtnLiga.innerHTML="Liga";										
 					displayMotor.appendChild(display_motorBtnLiga);
 						var display_motorBtnDesl=document.createElement("button");
 						display_motorBtnDesl.setAttribute("class","motor_btnDesl");
 						display_motorBtnDesl.setAttribute("id",displayMotorId+"-desl");
-						display_motorBtnDesl.innerHTML="Desl.";
+						display_motorBtnDesl.innerHTML="Desl.";						
 						displayMotor.appendChild(display_motorBtnDesl);
 							var display_motorBtnAuto=document.createElement("button");
 							display_motorBtnAuto.setAttribute("class","motor_btnAuto");
 							display_motorBtnAuto.setAttribute("id",displayMotorId+"-auto")
-							display_motorBtnAuto.innerHTML="Auto";
+							display_motorBtnAuto.innerHTML="Auto";							
 							displayMotor.appendChild(display_motorBtnAuto);
 								var display_motorBtnMan=document.createElement("button");
 								display_motorBtnMan.setAttribute("class","motor_btnMan");
 								display_motorBtnMan.setAttribute("id",displayMotorId+"-man")
-								display_motorBtnMan.innerHTML="Man";
+								display_motorBtnMan.innerHTML="Man";								
 								displayMotor.appendChild(display_motorBtnMan);
 		displayBtnAutoMan(displayMotorId);
-		displayBtnLigaDesl(displayMotorId);	
-								
-
-
+		displayBtnLigaDesl(displayMotorId);		
 
 		});		
 	}		
 }		
+
+
 
 function posMouse(){
 	document.querySelector('body').addEventListener('mousemove', function(event) {
@@ -75,58 +76,185 @@ function clicarArrastar(){
 			e.dataTransfer.setData("Text",e.target.id);			
 		});
 		}
-
 		dropZones.addEventListener("dragover", e=>{
 			e.preventDefault();
-		});
-		
+		});		
 		dropZones.addEventListener("drop", e=>{
 			const id=e.dataTransfer.getData("Text");			
 			const element=document.getElementById(id);			
 			e.target.appendChild(element);
 		});
 }		
-
+//****************Cria os eventos para os botões Auto e Man *********************
 function displayBtnAutoMan(id){		
 	var btnAuto=document.getElementById(id+"-auto");
-	var btnMan=document.getElementById(id+"-man");
+	var btnMan=document.getElementById(id+"-man");	 
+	
 	btnAuto.addEventListener("click",function(){
 		btnAuto.style.backgroundColor="green";
-		btnAuto.value="true";
+		btnAuto.value="true";		
 		btnMan.style.backgroundColor="rgb(100,100,100)";
-		btnMan.value="false";
-		acionaMotor(btnAuto);/*envia o id mt-__-auto para a função com seus atributos*/
+		btnMan.value="false";		
+		logicaMotores(this);
 	});
 	btnMan.addEventListener("click",function(){
 		btnAuto.style.backgroundColor="rgb(100,100,100)";
-		btnAuto.value="false";
+		btnAuto.value="false";		
 		btnMan.style.backgroundColor="green";
-		btnMan.value="true";
-		acionaMotor(btnMan);/*envia o id mt-__-man para a função com seus atributos*/
-	});
-	
+		btnMan.value="true";		
+		logicaMotores(this);
+	});	
 }
-
+//****************Cria os eventos para os botões Liga e Desl *********************
 function displayBtnLigaDesl(id){		
 	var btnLiga=document.getElementById(id+"-liga");
 	var btnDesl=document.getElementById(id+"-desl");
+	
 	btnLiga.addEventListener("click",function(id){
 		btnLiga.style.backgroundColor="green";
 		btnLiga.value="true";
 		btnDesl.style.backgroundColor="rgb(55,55,55)";
 		btnDesl.value="false";		
-		acionaMotor(btnLiga);/*envia o id mt-__-liga para a função com seus atributos*/
+		logicaMotores(this);
 	});
 	btnDesl.addEventListener("click",function(id){
 		btnLiga.style.backgroundColor="rgb(55,55,55)";
 		btnLiga.value="false";
 		btnDesl.style.backgroundColor="green";
-		btnDesl.value="true";
-		acionaMotor(btnDesl);/*envia o id mt-__-desl para a função com seus atributos*/
+		btnDesl.value="true";		
+		logicaMotores(this);
 	});
 	
 }
+*/
 
-function acionaMotor(id){	
 
-}
+/*
+function acionaMotor(ID){
+	
+
+//*****************lógica de acionamento mt-01 - motor mexedor do tanque de SI******************
+		
+	var mt01Aciona=document.getElementById("mt01Acionado");
+	var mt01Man=document.getElementById("mt-01-man");
+	var mt01Auto=document.getElementById("mt-01-auto");
+	var mt01Liga=document.getElementById("mt-01-liga");
+	var mt01Desl=document.getElementById("mt-01-desl");
+
+		if((mt01Man.value=="true")&&(mt01Liga.value=="true")){		
+			mt01Aciona.style.display="unset";			
+		}else mt01Aciona.style.display="none";
+
+//*****************lógica de acionamento mt-02 - motor bomba do tanque de SI**********************	
+	var mt02Aciona=document.getElementById("mt02Acionado");
+	var mt02Man=document.getElementById("mt-02-man");
+	var mt02Auto=document.getElementById("mt-02-auto");
+	var mt02Liga=document.getElementById("mt-02-liga");
+	var mt02Desl=document.getElementById("mt-02-desl");
+
+		if((mt02Man.value=="true")&&(mt02Liga.value=="true")){		
+			mt02Aciona.style.display="unset";			
+		}else mt02Aciona.style.display="none";
+}*/
+
+//********************* Motor Mexedor SI mt-01 ****************************
+
+var btnMt01Liga=document.getElementById("mt_01_liga");
+var btnMt01Desl=document.getElementById("mt_01_desl");
+var btnMt01Auto=document.getElementById("mt_01_auto");
+var btnMt01Man=document.getElementById("mt_01_man");
+var displayMt01=document.getElementById("display_mt01");
+var btnDisplayMt01=document.getElementById("mt_01");
+var btnMt01Close=document.getElementById("btn_mt01_close");
+var mt01Ligado=document.getElementById("mt01Acionado");
+
+		btnDisplayMt01.addEventListener("click",function(){
+			displayMt01.style.display="block";
+		});
+
+		btnMt01Auto.addEventListener("click",function(){
+			btnMt01Auto.style.backgroundColor="green";
+			btnMt01Auto.value="1";		
+			btnMt01Man.style.backgroundColor="rgb(100,100,100)";
+			btnMt01Man.value="0";
+			mt01Manual();					
+		})
+		btnMt01Man.addEventListener("click",function(){
+			btnMt01Auto.style.backgroundColor="rgb(100,100,100)";
+			btnMt01Auto.value="0";		
+			btnMt01Man.style.backgroundColor="green";
+			btnMt01Man.value="1";
+			mt01Manual();					
+		});	
+		btnMt01Liga.addEventListener("click",function(){
+			btnMt01Liga.style.backgroundColor="green";
+			btnMt01Liga.value="1";
+			btnMt01Desl.style.backgroundColor="rgb(55,55,55)";
+			btnMt01Desl.value="0";
+			mt01Manual();					
+		});
+		btnMt01Desl.addEventListener("click",function(){
+			btnMt01Liga.style.backgroundColor="rgb(55,55,55)";
+			btnMt01Liga.value="0";
+			btnMt01Desl.style.backgroundColor="green";
+			btnMt01Desl.value="1";
+			mt01Manual();					
+		});
+	
+	
+	btnMt01Close.addEventListener("click",function(){
+		displayMt01.style.display="none";
+	});
+
+// Acionamento manual do mexedor mt-01
+
+function mt01Manual(){	
+	if((btnMt01Man.value==="1" )&&(btnMt01Liga.value==="1")){
+		mt01Ligado.style.display="block";		
+	}else {
+	mt01Ligado.style.display="none"};	
+	}	 
+	
+
+
+//********************* Motor Bomba de SI mt-02 ****************************
+	var btnMt02Liga=document.getElementById("mt_02_liga");
+var btnMt02Desl=document.getElementById("mt_02_desl");
+var btnMt02Auto=document.getElementById("mt_02_auto");
+var btnMt02Man=document.getElementById("mt_02_man");
+var displayMt02=document.getElementById("display_mt02");
+var btnDisplayMt02=document.getElementById("mt_02");
+var btnMt02Close=document.getElementById("btn_mt02_close");
+
+	btnDisplayMt02.addEventListener("click",function(){
+		displayMt02.style.display="block";
+
+		btnMt02Auto.addEventListener("click",function(){
+			btnMt02Auto.style.backgroundColor="green";
+			btnMt02Auto.value="true";		
+			btnMt02Man.style.backgroundColor="rgb(100,100,100)";
+			btnMt02Man.value="false";		
+		});
+		btnMt02Man.addEventListener("click",function(){
+			btnMt02Auto.style.backgroundColor="rgb(100,100,100)";
+			btnMt02Auto.value="false";		
+			btnMt02Man.style.backgroundColor="green";
+			btnMt02Man.value="true";		
+		});	
+		btnMt02Liga.addEventListener("click",function(id){
+			btnMt02Liga.style.backgroundColor="green";
+			btnMt02Liga.value="true";
+			btnMt02Desl.style.backgroundColor="rgb(55,55,55)";
+			btnMt02Desl.value="false";		
+		});
+		btnMt02Desl.addEventListener("click",function(id){
+			btnMt02Liga.style.backgroundColor="rgb(55,55,55)";
+			btnMt02Liga.value="false";
+			btnMt02Desl.style.backgroundColor="green";
+			btnMt02Desl.value="true";		
+		});	
+	});
+	
+	btnMt02Close.addEventListener("click",function(){
+		displayMt02.style.display="none";		
+	});
